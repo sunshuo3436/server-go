121095434cfb53c8ce7740b88dfc36a81b12ade3
sunshuo3436 <146920157+sunshuo3436@users.noreply.github.com>
2024-04-27 20:47:29 +0800

    Update main.go

--- server/main.go
+++ server/main.go
@@ -1,26 +1,30 @@
+//远程过程调用RPC
 package main
 
 import (
 	"fmt"
 	"net/http"
-
 	"github.com/gorilla/mux"
 	"github.com/gorilla/rpc"
 	"github.com/gorilla/rpc/json"
+	/*Uses the Gorilla Mux and Gorilla RPC libraries 
+	to implement a JSON-RPC based Remote Procedure Call (RPC) service.
+        */
 )
 
 type SmsArgs struct {
 	Number, Content string
-}
+}//Parameters of SMS
 
 type EmailArgs struct {
 	To, Subject, Content string
-}
+}//Parameters of email
 
 type Response struct {
 	Result string
-}
+}//Parameters of response
 
+//Define the service
 type SmsService struct{}
 type EmailService struct{}
 
@@ -28,7 +32,10 @@ func (t *SmsService) SendSMS(r *http.Request, args *SmsArgs, result *Response) e
 	*result = Response{Result: fmt.Sprintf("Sms sent to %s", args.Number)}
 	return nil
 }
-
+/*
+args *SmsArgs :Arg for sending SMS, 
+contains the number of the SMS recipient and the content of the SMS.
+*/
 func (t *EmailService) SendEmail(r *http.Request, args *EmailArgs, result *Response) error {
 	*result = Response{Result: fmt.Sprintf("Email sent to %s", args.To)}
 	return nil
@@ -36,13 +43,16 @@ func (t *EmailService) SendEmail(r *http.Request, args *EmailArgs, result *Respo
 
 func main() {
 	rpcServer := rpc.NewServer()
-
+	
+	//Register Codecs
 	rpcServer.RegisterCodec(json.NewCodec(), "application/json")
 	rpcServer.RegisterCodec(json.NewCodec(), "application/json;charset=UTF-8")
-
+	
+       	//new():Creates a zero value of this type
 	sms := new(SmsService)
 	email := new(EmailService)
-
+	
+	//用于将请求路由到相应的服务处理函数。
 	rpcServer.RegisterService(sms, "sms")
 	rpcServer.RegisterService(email, "email")
 
