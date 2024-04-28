9f0e6828a377a5cd06130014bc34a1329462cf54
sunshuo3436 <146920157+sunshuo3436@users.noreply.github.com>
2024-04-28 21:56:20 +0800

    Update main.go

--- server/main.go
+++ server/main.go
@@ -1,220 +1,87 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
-	_ "github.com/mattn/go-sqlite3" // Import go-sqlite3 library
-	"log"
+	"errors"
 	"net"
 	"net/http"
 	"net/rpc"
-	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/swag/example/celler/controller"
+	"github.com/swaggo/swag/example/celler/httputil"
 )
 
-type StudentService struct {
-	DB *sql.DB
+type Args struct {
+	A, B int
 }
 
-type Student struct {
-	ID      int    `json:"id"`
-	Code    string `json:"code"`
-	Name    string `json:"name"`
-	Program string `json:"program"`
-}
+type Arith int
 
-type RPCRequest struct {
-	Method string        `json:"method"`
-	Params []interface{} `json:"params"`
-	ID     int           `json:"id"`
-}
-
-type RPCResponse struct {
-	Result interface{} `json:"result"`
-	Error  string      `json:"error,omitempty"`
-	ID     int         `json:"id"`
-}
-
-func (s *StudentService) HandleRequest(req RPCRequest, res *RPCResponse) error {
-	switch req.Method {
-	case "InsertStudent":
-		if len(req.Params) != 3 {
-			res.Error = "Invalid number of parameters"
-			return nil
-		}
-		code, ok := req.Params[0].(string)
-		if !ok {
-			res.Error = "Code parameter must be a string"
-			return nil
-		}
-		name, ok := req.Params[1].(string)
-		if !ok {
-			res.Error = "Name parameter must be a string"
-			return nil
-		}
-		program, ok := req.Params[2].(string)
-		if !ok {
-			res.Error = "Program parameter must be a string"
-			return nil
-		}
-		s.insertStudent(code, name, program)
-		res.Result = "Student inserted successfully"
-	case "DisplayStudents":
-		students, err := s.displayStudents()
-		if err != nil {
-			res.Error = err.Error()
-			return nil
-		}
-		res.Result = students
-	default:
-		res.Error = "Method not found"
-	}
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
 	return nil
 }
 
-func (s *StudentService) insertStudent(code, name, program string) {
-	log.Println("Inserting student record ...")
-	insertStudentSQL := `INSERT INTO student(code, name, program) VALUES (?, ?, ?)`
-	statement, err := s.DB.Prepare(insertStudentSQL)
-	if err != nil {
-		log.Println("Error preparing statement:", err)
-		return
-	}
-	defer statement.Close()
-
-	_, err = statement.Exec(code, name, program)
-	if err != nil {
-		log.Println("Error inserting student:", err)
-		return
-	}
-	log.Println("Student inserted successfully")
-}
-
-func (s *StudentService) displayStudents() ([]Student, error) {
-	log.Println("Displaying students ...")
-	rows, err := s.DB.Query("SELECT * FROM student ORDER BY name")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var students []Student
-	for rows.Next() {
-		var student Student
-		err := rows.Scan(&student.ID, &student.Code, &student.Name, &student.Program)
-		if err != nil {
-			return nil, err
-		}
-		students = append(students, student)
-	}
-	log.Println("Students displayed successfully")
-	return students, nil
-}
-
 func main() {
-	os.Remove("sqlite-database.db") // I delete the file to avoid duplicated records.
 
-	log.Println("Creating sqlite-database.db...")
-	file, err := os.Create("sqlite-database.db") // Create SQLite file
-	if err != nil {
-		log.Fatal(err.Error())
+	r := gin.Default()              //r用gin引擎
+	c := controller.NewController() //c用controller
+
+	v1 := r.Group("/api/v1") // 路由组v1，包含 API 的版本前缀 /api/v1
+	{
+		accounts := v1.Group("/accounts")
+		{
+			accounts.GET(":id", c.ShowAccount)
+			accounts.GET("", c.ListAccounts)
+			accounts.POST("", c.AddAccount)
+			accounts.DELETE(":id", c.DeleteAccount)
+			accounts.PATCH(":id", c.UpdateAccount)
+			accounts.POST(":id/images", c.UploadAccountImage)
+		}
+		bottles := v1.Group("/bottles")
+		{
+			bottles.GET(":id", c.ShowBottle)
+			bottles.GET("", c.ListBottles)
+		}
+		admin := v1.Group("/admin")
+		{
+			admin.Use(auth())
+			admin.POST("/auth", c.Auth)
+		}
+		examples := v1.Group("/examples")
+		{
+			examples.GET("ping", c.PingExample)
+			examples.GET("calc", c.CalcExample)
+			examples.GET("groups/:group_id/accounts/:account_id", c.PathParamsExample)
+			examples.GET("header", c.HeaderExample)
+			examples.GET("securities", c.SecuritiesExample)
+			examples.GET("attribute", c.AttributeExample)
+		}
 	}
-	file.Close()
-	log.Println("sqlite-database.db created")
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Run(":8080")
 
-	sqliteDatabase, err := sql.Open("sqlite3", "./sqlite-database.db") // Open the created SQLite File
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer sqliteDatabase.Close() // Defer Closing the database
-	createTable(sqliteDatabase)  // Create Database Tables
-
-	// INSERT RECORDS
-	studentService := &StudentService{DB: sqliteDatabase}
-	studentService.insertStudent("0001", "Liana Kim", "Bachelor")
-	studentService.insertStudent("0002", "Glen Rangel", "Bachelor")
-	studentService.insertStudent("0003", "Martin Martins", "Master")
-	studentService.insertStudent("0004", "Alayna Armitage", "PHD")
-	studentService.insertStudent("0005", "Marni Benson", "Bachelor")
-	studentService.insertStudent("0006", "Derrick Griffiths", "Master")
-	studentService.insertStudent("0007", "Leigh Daly", "Bachelor")
-	studentService.insertStudent("0008", "Marni Benson", "PHD")
-	studentService.insertStudent("0009", "Klay Correa", "Bachelor")
-
-	// DISPLAY INSERTED RECORDS
-	displayStudents(sqliteDatabase)
-
-	// Setup RPC
-	rpc.Register(studentService)
+	arith := new(Arith)
+	rpc.Register(arith)
 	rpc.HandleHTTP()
 
-	// Start RPC server
-	listener, err := net.Listen("tcp", ":1234")
+	l, err := net.Listen("tcp", ":1234")
 	if err != nil {
-		log.Fatal("Error starting RPC server:", err)
+		panic(err)
 	}
-	log.Println("RPC server started on port 1234")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		var req RPCRequest
-		err := json.NewDecoder(r.Body).Decode(&req)
-		if err != nil {
-			http.Error(w, "Invalid request body", http.StatusBadRequest)
-			return
-		}
-
-		var res RPCResponse
-		err = studentService.HandleRequest(req, &res)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		json.NewEncoder(w).Encode(res)
-	})
-
-	log.Fatal(http.Serve(listener, nil))
+	http.Serve(l, nil)
 }
 
-func createTable(db *sql.DB) {
-	createStudentTableSQL := `CREATE TABLE student (
-		"idStudent" integer NOT NULL PRIMARY KEY AUTOINCREMENT,		
-		"code" TEXT,
-		"name" TEXT,
-		"program" TEXT		
-	  );` // SQL Statement for Create Table
-
-	log.Println("Create student table...")
-	statement, err := db.Prepare(createStudentTableSQL) // Prepare SQL Statement
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	statement.Exec() // Execute SQL Statements
-	log.Println("student table created")
-}
-
-func displayStudents(db *sql.DB) {
-	log.Println("Displaying students ...")
-	rows, err := db.Query("SELECT * FROM student ORDER BY name")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var id int
-		var code string
-		var name string
-		var program string
-		err := rows.Scan(&id, &code, &name, &program)
-		if err != nil {
-			log.Fatal(err)
+func auth() gin.HandlerFunc { //用于检查请求中是否包含授权信息
+	return func(c *gin.Context) {
+		if len(c.GetHeader("Authorization")) == 0 {
+			httputil.NewError(c, http.StatusUnauthorized, errors.New("Authorization is required Header"))
+			c.Abort()
 		}
-		log.Println("Student: ", code, " ", name, " ", program)
+		c.Next()
 	}
-	log.Println("Students displayed successfully")
 }
