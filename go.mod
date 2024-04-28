51f34e4ca5142a78ed4144ef3381da50ed68ec66
sunshuo <sunshuo3436@gmail.com>
2024-04-28 19:46:20 +0800

    swaggerrestapi

--- go.mod
+++
@@ -1,9 +0,0 @@
-module server-go
-
-go 1.21.1
-
-require (
-	github.com/gorilla/mux v1.8.1
-	github.com/gorilla/rpc v1.2.1
-	github.com/mattn/go-sqlite3 v1.14.22
-)
--- server/main.go
+++ server/main.go
@@ -1,220 +1,107 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
-	_ "github.com/mattn/go-sqlite3" // Import go-sqlite3 library
-	"log"
-	"net"
+	"errors"
 	"net/http"
-	"net/rpc"
-	"os"
+
+	"github.com/gin-gonic/gin"                         //Gin framework for building web applications
+	"github.com/swaggo/swag/example/celler/controller" //controller package for the example application
+	_ "github.com/swaggo/swag/example/celler/docs"
+	"github.com/swaggo/swag/example/celler/httputil"
+
+	swaggerFiles "github.com/swaggo/files"     //Integrating Swagger Documentation with the Gin Framework
+	ginSwagger "github.com/swaggo/gin-swagger" //Integrating Swagger Documentation with the Gin Framework
 )
 
-type StudentService struct {
-	DB *sql.DB
-}
+//API 的元信息，包括标题、版本、描述、联系方式、许可证、主机、基础路径和安全定义等
+//	@title			Swagger Example API
+//	@version		1.0
+//	@description	This is a sample server celler server.
+//	@termsOfService	http://swagger.io/terms/
 
-type Student struct {
-	ID      int    `json:"id"`
-	Code    string `json:"code"`
-	Name    string `json:"name"`
-	Program string `json:"program"`
-}
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
 
-type RPCRequest struct {
-	Method string        `json:"method"`
-	Params []interface{} `json:"params"`
-	ID     int           `json:"id"`
-}
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
 
-type RPCResponse struct {
-	Result interface{} `json:"result"`
-	Error  string      `json:"error,omitempty"`
-	ID     int         `json:"id"`
-}
+//	@host		localhost:8080
+//	@BasePath	/api/v1
 
-func (s *StudentService) HandleRequest(req RPCRequest, res *RPCResponse) error {
-	switch req.Method {
-	case "InsertStudent":
-		if len(req.Params) != 3 {
-			res.Error = "Invalid number of parameters"
-			return nil
-		}
-		code, ok := req.Params[0].(string)
-		if !ok {
-			res.Error = "Code parameter must be a string"
-			return nil
-		}
-		name, ok := req.Params[1].(string)
-		if !ok {
-			res.Error = "Name parameter must be a string"
-			return nil
-		}
-		program, ok := req.Params[2].(string)
-		if !ok {
-			res.Error = "Program parameter must be a string"
-			return nil
-		}
-		s.insertStudent(code, name, program)
-		res.Result = "Student inserted successfully"
-	case "DisplayStudents":
-		students, err := s.displayStudents()
-		if err != nil {
-			res.Error = err.Error()
-			return nil
-		}
-		res.Result = students
-	default:
-		res.Error = "Method not found"
-	}
-	return nil
-}
+//	@securityDefinitions.basic	BasicAuth
 
-func (s *StudentService) insertStudent(code, name, program string) {
-	log.Println("Inserting student record ...")
-	insertStudentSQL := `INSERT INTO student(code, name, program) VALUES (?, ?, ?)`
-	statement, err := s.DB.Prepare(insertStudentSQL)
-	if err != nil {
-		log.Println("Error preparing statement:", err)
-		return
-	}
-	defer statement.Close()
+//	@securityDefinitions.apikey	ApiKeyAuth
+//	@in							header
+//	@name						Authorization
+//	@description				Description for what is this security definition being used
 
-	_, err = statement.Exec(code, name, program)
-	if err != nil {
-		log.Println("Error inserting student:", err)
-		return
-	}
-	log.Println("Student inserted successfully")
-}
+//	@securitydefinitions.oauth2.application	OAuth2Application
+//	@tokenUrl								https://example.com/oauth/token
+//	@scope.write							Grants write access
+//	@scope.admin							Grants read and write access to administrative information
 
-func (s *StudentService) displayStudents() ([]Student, error) {
-	log.Println("Displaying students ...")
-	rows, err := s.DB.Query("SELECT * FROM student ORDER BY name")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
+//	@securitydefinitions.oauth2.implicit	OAuth2Implicit
+//	@authorizationUrl						https://example.com/oauth/authorize
+//	@scope.write							Grants write access
+//	@scope.admin							Grants read and write access to administrative information
 
-	var students []Student
-	for rows.Next() {
-		var student Student
-		err := rows.Scan(&student.ID, &student.Code, &student.Name, &student.Program)
-		if err != nil {
-			return nil, err
-		}
-		students = append(students, student)
-	}
-	log.Println("Students displayed successfully")
-	return students, nil
-}
+//	@securitydefinitions.oauth2.password	OAuth2Password
+//	@tokenUrl								https://example.com/oauth/token
+//	@scope.read								Grants read access
+//	@scope.write							Grants write access
+//	@scope.admin							Grants read and write access to administrative information
+
+//	@securitydefinitions.oauth2.accessCode	OAuth2AccessCode
+//	@tokenUrl								https://example.com/oauth/token
+//	@authorizationUrl						https://example.com/oauth/authorize
+//	@scope.admin							Grants read and write access to administrative information
 
 func main() {
-	os.Remove("sqlite-database.db") // I delete the file to avoid duplicated records.
+	r := gin.Default()              //r用gin引擎
+	c := controller.NewController() //c用controller
 
-	log.Println("Creating sqlite-database.db...")
-	file, err := os.Create("sqlite-database.db") // Create SQLite file
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	file.Close()
-	log.Println("sqlite-database.db created")
-
-	sqliteDatabase, err := sql.Open("sqlite3", "./sqlite-database.db") // Open the created SQLite File
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer sqliteDatabase.Close() // Defer Closing the database
-	createTable(sqliteDatabase)  // Create Database Tables
-
-	// INSERT RECORDS
-	studentService := &StudentService{DB: sqliteDatabase}
-	studentService.insertStudent("0001", "Liana Kim", "Bachelor")
-	studentService.insertStudent("0002", "Glen Rangel", "Bachelor")
-	studentService.insertStudent("0003", "Martin Martins", "Master")
-	studentService.insertStudent("0004", "Alayna Armitage", "PHD")
-	studentService.insertStudent("0005", "Marni Benson", "Bachelor")
-	studentService.insertStudent("0006", "Derrick Griffiths", "Master")
-	studentService.insertStudent("0007", "Leigh Daly", "Bachelor")
-	studentService.insertStudent("0008", "Marni Benson", "PHD")
-	studentService.insertStudent("0009", "Klay Correa", "Bachelor")
-
-	// DISPLAY INSERTED RECORDS
-	displayStudents(sqliteDatabase)
-
-	// Setup RPC
-	rpc.Register(studentService)
-	rpc.HandleHTTP()
-
-	// Start RPC server
-	listener, err := net.Listen("tcp", ":1234")
-	if err != nil {
-		log.Fatal("Error starting RPC server:", err)
-	}
-	log.Println("RPC server started on port 1234")
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
+	v1 := r.Group("/api/v1") // 路由组v1，包含 API 的版本前缀 /api/v1
+	{
+		accounts := v1.Group("/accounts")
+		{
+			accounts.GET(":id", c.ShowAccount)
+			accounts.GET("", c.ListAccounts)
+			accounts.POST("", c.AddAccount)
+			accounts.DELETE(":id", c.DeleteAccount)
+			accounts.PATCH(":id", c.UpdateAccount)
+			accounts.POST(":id/images", c.UploadAccountImage)
 		}
-
-		var req RPCRequest
-		err := json.NewDecoder(r.Body).Decode(&req)
-		if err != nil {
-			http.Error(w, "Invalid request body", http.StatusBadRequest)
-			return
+		bottles := v1.Group("/bottles")
+		{
+			bottles.GET(":id", c.ShowBottle)
+			bottles.GET("", c.ListBottles)
 		}
-
-		var res RPCResponse
-		err = studentService.HandleRequest(req, &res)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+		admin := v1.Group("/admin")
+		{
+			admin.Use(auth())
+			admin.POST("/auth", c.Auth)
 		}
-
-		json.NewEncoder(w).Encode(res)
-	})
-
-	log.Fatal(http.Serve(listener, nil))
+		examples := v1.Group("/examples")
+		{
+			examples.GET("ping", c.PingExample)
+			examples.GET("calc", c.CalcExample)
+			examples.GET("groups/:group_id/accounts/:account_id", c.PathParamsExample)
+			examples.GET("header", c.HeaderExample)
+			examples.GET("securities", c.SecuritiesExample)
+			examples.GET("attribute", c.AttributeExample)
+		}
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Run(":8080")
 }
 
-func createTable(db *sql.DB) {
-	createStudentTableSQL := `CREATE TABLE student (
-		"idStudent" integer NOT NULL PRIMARY KEY AUTOINCREMENT,		
-		"code" TEXT,
-		"name" TEXT,
-		"program" TEXT		
-	  );` // SQL Statement for Create Table
-
-	log.Println("Create student table...")
-	statement, err := db.Prepare(createStudentTableSQL) // Prepare SQL Statement
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	statement.Exec() // Execute SQL Statements
-	log.Println("student table created")
-}
-
-func displayStudents(db *sql.DB) {
-	log.Println("Displaying students ...")
-	rows, err := db.Query("SELECT * FROM student ORDER BY name")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var id int
-		var code string
-		var name string
-		var program string
-		err := rows.Scan(&id, &code, &name, &program)
-		if err != nil {
-			log.Fatal(err)
+func auth() gin.HandlerFunc { //用于检查请求中是否包含授权信息
+	return func(c *gin.Context) {
+		if len(c.GetHeader("Authorization")) == 0 {
+			httputil.NewError(c, http.StatusUnauthorized, errors.New("Authorization is required Header"))
+			c.Abort()
 		}
-		log.Println("Student: ", code, " ", name, " ", program)
+		c.Next()
 	}
-	log.Println("Students displayed successfully")
 }
